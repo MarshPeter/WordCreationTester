ac3aac12553234e7bfe1f295ce5bb3ae479cbfe2
Peter Farmer <peter.m.farmer.204@gmail.com>
2025-04-24 00:28:37 +1000

    AI Search working through multiple indexes now.

--- WordCreationTester/Program.cs
+++ WordCreationTester/Program.cs
@@ -3,12 +3,88 @@ using Newtonsoft.Json;
 using Openize.Words;
 using Openize.Words.IElements;
 using WordCreationTester;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.OpenAI;
+using Azure.AI.OpenAI.Chat;
+using Azure.Identity;
+using OpenAI.Chat;
+using static System.Environment;
+using Microsoft.VisualBasic;
+using System.ComponentModel;
+using DocumentFormat.OpenXml.Drawing.Charts;
 
-string docsDirectory = "./docs";
-string filename = "Generated.docx";
+runAI();
 
-// JSON string representing the report data
-string jsonString = @"{
+static void runAI()
+{
+
+    string AI_endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? "https://<your-resource-name>.openai.azure.com/";
+    string AI_key = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY") ?? "<your-key>";
+    string searchEndpoint = GetEnvironmentVariable("AZURE_AI_SEARCH_ENDPOINT");
+    string searchKey = Environment.GetEnvironmentVariable("AZURE_SEARCH_API_KEY") ?? "<your-search-api-key>";
+
+
+    var searchIndexes = new List<string>
+        {
+            "gptindex",
+            "incident-index",
+        };
+
+    var responses = new List<string> { };
+
+    foreach (String index in searchIndexes)
+    {
+        try
+        {
+            // Use the recommended keyless credential instead of the AzureKeyCredential credential.
+            // AzureOpenAIClient openAIClient = new AzureOpenAIClient(new Uri(endpoint), new DefaultAzureCredential());
+            AzureOpenAIClient openAIClient = new AzureOpenAIClient(new Uri(AI_endpoint), new AzureKeyCredential(AI_key));
+
+            // This must match the custom deployment name you chose for your model
+            ChatClient chatClient = openAIClient.GetChatClient("gpt-4o-mini");
+
+            ChatCompletionOptions options = new ChatCompletionOptions();
+
+#pragma warning disable AOAI001 // Suppress the diagnostic warning  
+
+            options.AddDataSource(new AzureSearchChatDataSource()
+            {
+                Endpoint = new Uri(searchEndpoint),
+                IndexName = index,
+                Authentication = DataSourceAuthentication.FromApiKey(searchKey), // Add your Azure AI Search admin key here  
+            });
+
+            Console.WriteLine(options.GetDataSources());
+
+            ChatCompletion completion = chatClient.CompleteChat([
+                new SystemChatMessage("You are a helpful assistant that collects information that we request by looking at the provided datasources. If you find nothing of relevance in the datasource, just return an empty string and nothing else. This is fine and shouldn't be seen as an error."),
+                new UserChatMessage("Show all incidents involving luke")
+            ], options);
+
+            responses.Add(completion.Content[0].Text);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("nothing found here.");
+        }
+    }
+
+    foreach (string res in responses)
+    {
+        Console.WriteLine(res);
+    }
+}
+
+static void runGeneration()
+{
+    string docsDirectory = "./docs";
+    string filename = "Generated.docx";
+
+    // JSON string representing the report data
+    string jsonString = @"{
           ""reportTitle"": ""The Humble Dandelion: More Than Just a Weed"",
           ""sections"": [
             {
@@ -457,53 +533,58 @@ string jsonString = @"{
           ]
         }";
 
-try
-{
-    // Deserialize the JSON string into a Report object
-    Report report = JsonConvert.DeserializeObject<Report>(jsonString);
-
-    var doc = new Document();
-
-    var word = new WordFileGenerator(doc);
-
-    word.addTitle(report.ReportTitle);
-
-    foreach (Section s in report.Sections)
+    try
     {
-        ParseSection(word, s);
+        // Deserialize the JSON string into a Report object
+        Report report = JsonConvert.DeserializeObject<Report>(jsonString);
+
+        var doc = new Document();
+
+        var word = new WordFileGenerator(doc);
+
+        word.addTitle(report.ReportTitle);
+
+        foreach (Section s in report.Sections)
+        {
+            ParseSection(word, s);
+        }
+
+        doc.Save($"{docsDirectory}/{filename}");
+    }
+    catch (System.Exception ex)
+    {
+        throw new FileFormatException("An error occurred.", ex);
     }
 
-    doc.Save($"{docsDirectory}/{filename}");
-}
-catch (System.Exception ex)
-{
-    throw new FileFormatException("An error occurred.", ex);
-}
+    static void ParseSection(WordFileGenerator w, Section s)
+    {
+        if (s.SectionTitle != "")
+        {
+            w.addHeader(s.SectionTitle);
+        }
 
-static void ParseSection(WordFileGenerator w, Section s)
-{
-    if (s.SectionTitle != "")
-    {
-        w.addHeader(s.SectionTitle);
-    }
+        if (s.SectionContext.Equals("paragraph"))
+        {
+            parseNormalSection(w, s);
+        }
+        else if (s.SectionContext.Equals("table"))
+        {
+            parseTable(w, s);
+        }
+        else if (s.SectionContext.Equals("dotpoint"))
+        {
+            parseDotPoints(w, s);
+        }
+        else if (s.SectionContext.Equals("numberList"))
+        {
+            parseNumberList(w, s);
+        }
 
-    if (s.SectionContext.Equals("paragraph"))
-    {
-        parseNormalSection(w, s);
-    } else if (s.SectionContext.Equals("table"))
-    {
-        parseTable(w, s);
-    } else if (s.SectionContext.Equals("dotpoint"))
-    {
-        parseDotPoints(w, s);
-    } else if (s.SectionContext.Equals("numberList"))
-    {
-        parseNumberList(w, s);
-    }
+        foreach (Section subSection in s.Sections)
+        {
+            ParseSection(w, subSection);
+        }
 
-    foreach (Section subSection in s.Sections)
-    {
-        ParseSection(w, subSection);
     }
 
 }
@@ -512,7 +593,7 @@ static void parseNormalSection(WordFileGenerator w, Section s)
 {
     if (s.ParagraphContext.Equals("normal") && s.Content != "")
     {
-        w.addParagraph(s.Content);   
+        w.addParagraph(s.Content);
     }
 }
 
@@ -540,7 +621,8 @@ static void parseDotPoints(WordFileGenerator w, Section s, int indent = 1)
     if (s.ParagraphContext.Equals("paragraph"))
     {
         w.addParagraph(s.Content);
-    } else if (s.ParagraphContext.Equals("dotpoint"))
+    }
+    else if (s.ParagraphContext.Equals("dotpoint"))
     {
         w.addDotpointParagraph(s.Content, indent);
     }
@@ -556,7 +638,8 @@ static void parseNumberList(WordFileGenerator w, Section s, int indent = 1, int 
     if (s.ParagraphContext.Equals("paragraph"))
     {
         w.addParagraph(s.Content);
-    } else if (s.ParagraphContext.Equals("numberList"))
+    }
+    else if (s.ParagraphContext.Equals("numberList"))
     {
         w.addNumericListParagraph(s.Content, number, indent);
     }
