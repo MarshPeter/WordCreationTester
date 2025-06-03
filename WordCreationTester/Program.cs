b4a0df7c04cc8042a0d26b6901792775f9acafc4
Peter Farmer <peter.m.farmer.204@gmail.com>
2025-06-03 15:44:56 +1000

    Fixed AI Chat so now that we can connect out vectorzied and semantically ranked hybrid searches

--- WordCreationTester/Program.cs
+++ WordCreationTester/Program.cs
@@ -9,7 +9,7 @@ using System.IdentityModel.Tokens.Jwt;
 // ReportCreator.runGeneration(JsonTests.jsonString);
 
 // Uncomment this to work with the AI code. 
-// AIRunner.runAI();
+await AIRunner.runAI();
 
 // Uncomment this to work with the AIService Creator
 // await AIServiceCreator.createSearchResource("swin-testing", "swin-testing-ai-programmatic");
@@ -19,42 +19,40 @@ using System.IdentityModel.Tokens.Jwt;
 
 // await IndexCreator.CreateSearchResourcesAsync();
 
+// THE FOLLOWING CODE IS AN EXAMPLE OF INDEXING CREATION
+//string searchServiceName = "swintesting-ai";
+//string dataSourceName = "my-datasource";
+//string skillsetName = "my-skillset";
+//string indexName = "my-index";
+//string indexerName = "my-indexer";
+//string blobConnectionString = Environment.GetEnvironmentVariable("BLOB_STORAGE_CONNECTION_STRING");
+//string containerName = "documents";
+//string openAIEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+//string openAIKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY");
 
 
-// THE FOLLOWING CODE IS FOR INDEXING
-string searchServiceName = "swintesting-ai";
-string dataSourceName = "my-datasource";
-string skillsetName = "my-skillset";
-string indexName = "my-index";
-string indexerName = "my-indexer";
-string blobConnectionString = Environment.GetEnvironmentVariable("BLOB_STORAGE_CONNECTION_STRING");
-string containerName = "documents";
-string openAIEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
-string openAIKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY");
+//string dataSourcePayload = $@"
+//{{
+//    ""name"": ""{dataSourceName}"",
+//    ""description"": ""Data source for blob container"",
+//    ""type"": ""azureblob"",
+//    ""credentials"": {{ ""connectionString"": ""{blobConnectionString}"" }},
+//    ""container"": {{ ""name"": ""{containerName}"" }}
+//}}";
 
+//// 1. Create Data Source (as before)
+//await AzureSearchDataSourceCreator.CreateDataSourceAsync(searchServiceName, dataSourcePayload);
 
-string dataSourcePayload = $@"
-{{
-    ""name"": ""{dataSourceName}"",
-    ""description"": ""Data source for blob container"",
-    ""type"": ""azureblob"",
-    ""credentials"": {{ ""connectionString"": ""{blobConnectionString}"" }},
-    ""container"": {{ ""name"": ""{containerName}"" }}
-}}";
+//// 3.Create Index
+//await AzureSearchDataSourceCreator.CreateIndexAsync(searchServiceName, indexName, openAIEndpoint, openAIKey);
 
-// 1. Create Data Source (as before)
-await AzureSearchDataSourceCreator.CreateDataSourceAsync(searchServiceName, dataSourcePayload);
+//// 2.Create Skillset
+//await AzureSearchDataSourceCreator.CreateSkillsetAsync(searchServiceName, skillsetName, openAIEndpoint, openAIKey, indexName);
 
-// 3.Create Index
-await AzureSearchDataSourceCreator.CreateIndexAsync(searchServiceName, indexName, openAIEndpoint, openAIKey);
-
-// 2.Create Skillset
-await AzureSearchDataSourceCreator.CreateSkillsetAsync(searchServiceName, skillsetName, openAIEndpoint, openAIKey, indexName);
-
-// 4.Create Indexer
-await AzureSearchDataSourceCreator.CreateIndexerAsync(
-    searchServiceName,
-   indexerName,
-   dataSourceName,
-   skillsetName,
-   indexName);
+//// 4.Create Indexer
+//await AzureSearchDataSourceCreator.CreateIndexerAsync(
+//    searchServiceName,
+//   indexerName,
+//   dataSourceName,
+//   skillsetName,
+//   indexName);
